d54d740ca1653d9df318ad9bcae7ca9de26d10c9
MKS-Student <mks.student@hackreactor.com>
2017-02-24 17:48:48 -0800

    got our schema working

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,81 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS messages;
+		
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  content MEDIUMTEXT NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  roomId INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (roomId) REFERENCES  rooms(id),
+  FOREIGN KEY (user_id) REFERENCES  users(id)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'users'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS users;
+		
+CREATE TABLE users (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (id)
+);
 
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS rooms;
+		
+CREATE TABLE rooms (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  roomName MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (id)
+);
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+-- ---
+-- Foreign Keys 
+-- ---
 
+-- ALTER TABLE `users` ADD FOREIGN KEY (id) REFERENCES `messages` (`user_id`);
+-- ALTER TABLE `rooms` ADD FOREIGN KEY (id) REFERENCES `messages` (`roomId`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`content`,`createdAt`,`roomId`,`user_id`) VALUES
+-- ('','','','','');
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`roomName`) VALUES
+-- ('','');
